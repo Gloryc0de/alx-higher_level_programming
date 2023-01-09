4b81ac4a2593d2f6bfe9d7801f016a2ff71a6505
Gloryc0de <glorysampson2024@gmail.com>
2023-01-09 22:09:18 +0000

    function that replaces an element from a list at specific position

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-def element_at(my_list, idx):
+def element_at(my_list, idx, element):
     i = len(my_list)
     if idx < 0:
         return (my_list)
     elif idx >= i:
-        return (my_list)
-    else:
-        return (my_list[idx])
+        return (my_list)	
