bd83e999c85f2805335c7d4032a582ef56a609b4
Gloryc0de <glorysampson2024@gmail.com>
2023-01-09 23:54:59 +0000

    functions that returns a turple with the length of a string and its first character

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
 # Returns a turple with the length of a string and its first chacter
-    string_length = len(sentence):
+    string_length = len(sentence)
         if string_lenth == 0:
             first_char = none
         else:
